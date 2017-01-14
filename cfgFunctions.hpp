03a5daf9d49f29e6dd1aa7ae8d3add0d0aacbe8c
ElTyranos <pierre.cazard@gmail.com>
2017-01-14 16:15:02 +0100

    ~ Fixed: function's path

--- cfgFunctions.hpp
+++ cfgFunctions.hpp
@@ -5,11 +5,11 @@ class CfgFunctions
 		tag = "BG21_IFA3";
 		class BG21_IFA3_Functions
 		{
-			file = "haxey_units\fnc";
+			file = "MG_AllInOne\fnc";
 			class random_gear
 			{
 				preInit = 1;
-			};			
+			};
 		};
 	};
 };
