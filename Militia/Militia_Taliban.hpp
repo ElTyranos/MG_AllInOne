eeea823573488dd46d761efe3aaf670a2a602712
ElTyranos <pierre.cazard@gmail.com>
2017-05-28 21:55:45 +0200

    ~ Changed: Talibans uniforms

--- Militia/Militia_Taliban.hpp
+++ Militia/Militia_Taliban.hpp
@@ -10,7 +10,7 @@ class haxey_Milice_Taliban_Infantry_Warlord: O_officer_F
 	faction = "haxey_faction_Milice_Taliban";
 	vehicleClass = "H_HQMen";
 	displayName = "Warlord";
-	uniformClass = "LOP_U_AM_Fatigue_01_2";
+	uniformClass = "btc_h_afghan_01";
 	class EventHandlers: EventHandlers
 	{
 		init = "if (local (_this select 0)) then {[(_this select 0), 'MIL_TAL'] call BG21_MG_fnc_random_gear;};";
@@ -40,7 +40,7 @@ class haxey_Milice_Taliban_Infantry_RH: O_Soldier_TL_F
 	faction = "haxey_faction_Milice_Taliban";
 	vehicleClass = "H_Men";
 	displayName = "Right-hand man";
-	uniformClass = "LOP_U_AM_Fatigue_01_2";
+	uniformClass = "btc_h_afghan_01";
 	class EventHandlers: EventHandlers
 	{
 		init = "if (local (_this select 0)) then {[(_this select 0), 'MIL_TAL'] call BG21_MG_fnc_random_gear;};";
@@ -51,8 +51,8 @@ class haxey_Milice_Taliban_Infantry_RH: O_Soldier_TL_F
 	respawnWeapons[] = {"rhs_weap_akm","rhs_weap_rshg2","Binocular","Throw","Put"};
 	magazines[] = {DOT_MIL_762LMG_0,GRE_MIL_0};
 	respawnmagazines[] = {DOT_MIL_762LMG_0,GRE_MIL_0};
-	linkedItems[] = {LINKED_ITEMS_MIL};
-	respawnLinkedItems[] = {LINKED_ITEMS_MIL};
+	linkedItems[] = {LINKED_ITEMS_BASE};
+	respawnLinkedItems[] = {LINKED_ITEMS_BASE};
 	backpack = "";
 };
 class haxey_Milice_Taliban_Infantry_AR: O_soldier_AR_F
@@ -66,7 +66,7 @@ class haxey_Milice_Taliban_Infantry_AR: O_soldier_AR_F
 	faction = "haxey_faction_Milice_Taliban";
 	vehicleClass = "H_Men";
 	displayName = "Merchant";
-	uniformClass = "LOP_U_AM_Fatigue_01_2";
+	uniformClass = "btc_h_afghan_01";
 	class EventHandlers: EventHandlers
 	{
 		init = "if (local (_this select 0)) then {[(_this select 0), 'MIL_TAL'] call BG21_MG_fnc_random_gear;};";
@@ -77,8 +77,8 @@ class haxey_Milice_Taliban_Infantry_AR: O_soldier_AR_F
 	respawnWeapons[] = {"hlc_rifle_rpk","Throw","Put"};
 	magazines[] = {DOT_MIL_762MG_0,GRE_MIL_0};
 	respawnMagazines[] = {DOT_MIL_762MG_0,GRE_MIL_0};
-	linkedItems[] = {LINKED_ITEMS_MIL};
-	respawnLinkedItems[] = {LINKED_ITEMS_MIL};
+	linkedItems[] = {LINKED_ITEMS_BASE};
+	respawnLinkedItems[] = {LINKED_ITEMS_BASE};
 	backpack = "";
 };
 class haxey_Milice_Taliban_Infantry_exp: O_soldier_exp_F
@@ -92,7 +92,7 @@ class haxey_Milice_Taliban_Infantry_exp: O_soldier_exp_F
 	faction = "haxey_faction_Milice_Taliban";
 	vehicleClass = "H_Men";
 	displayName = "Mechanic (exp)";
-	uniformClass = "LOP_U_AM_Fatigue_01_2";
+	uniformClass = "btc_h_afghan_01";
 	class EventHandlers: EventHandlers
 	{
 		init = "if (local (_this select 0)) then {[(_this select 0), 'MIL_TAL'] call BG21_MG_fnc_random_gear;};";
@@ -103,8 +103,8 @@ class haxey_Milice_Taliban_Infantry_exp: O_soldier_exp_F
 	respawnWeapons[] = {"rhs_weap_aks74u","Throw","Put"};
 	magazines[] = {DOT_MIL_545LMG_0,GRE_MIL_0};
 	respawnmagazines[] = {DOT_MIL_545LMG_0,GRE_MIL_0};
-	linkedItems[] = {LINKED_ITEMS_MIL};
-	respawnLinkedItems[] = {LINKED_ITEMS_MIL};
+	linkedItems[] = {LINKED_ITEMS_BASE};
+	respawnLinkedItems[] = {LINKED_ITEMS_BASE};
 	backpack = "haxey_B_FieldPack_cbr";
 };
 class haxey_Milice_Taliban_Infantry_LAT: O_soldier_LAT_F
@@ -118,7 +118,7 @@ class haxey_Milice_Taliban_Infantry_LAT: O_soldier_LAT_F
 	faction = "haxey_faction_Milice_Taliban";
 	vehicleClass = "H_Men";
 	displayName = "Villager Light-AT";
-	uniformClass = "LOP_U_AM_Fatigue_01_2";
+	uniformClass = "btc_h_afghan_01";
 	class EventHandlers: EventHandlers
 	{
 		init = "if (local (_this select 0)) then {[(_this select 0), 'MIL_TAL'] call BG21_MG_fnc_random_gear;};";
@@ -129,8 +129,8 @@ class haxey_Milice_Taliban_Infantry_LAT: O_soldier_LAT_F
 	respawnWeapons[] = {"rhs_weap_aks74u","rhs_weap_rpg26","Throw","Put"};
 	magazines[] = {DOT_MIL_545LMG_0,GRE_MIL_0};
 	respawnmagazines[] = {DOT_MIL_545LMG_0,GRE_MIL_0};
-	linkedItems[] = {LINKED_ITEMS_MIL};
-	respawnLinkedItems[] = {LINKED_ITEMS_MIL};
+	linkedItems[] = {LINKED_ITEMS_BASE};
+	respawnLinkedItems[] = {LINKED_ITEMS_BASE};
 	backpack = "";
 };
 class haxey_Milice_Taliban_Infantry_AT: O_soldier_AT_F
@@ -144,7 +144,7 @@ class haxey_Milice_Taliban_Infantry_AT: O_soldier_AT_F
 	faction = "haxey_faction_Milice_Taliban";
 	vehicleClass = "H_Men";
 	displayName = "Villager AT";
-	uniformClass = "LOP_U_AM_Fatigue_01_2";
+	uniformClass = "btc_h_afghan_01";
 	class EventHandlers: EventHandlers
 	{
 		init = "if (local (_this select 0)) then {[(_this select 0), 'MIL_TAL'] call BG21_MG_fnc_random_gear;};";
@@ -155,8 +155,8 @@ class haxey_Milice_Taliban_Infantry_AT: O_soldier_AT_F
 	respawnWeapons[] = {"rhs_weap_aks74u","rhs_weap_rpg7","Throw","Put"};
 	magazines[] = {DOT_MIL_545LMG_0,GRE_MIL_0};
 	respawnmagazines[] = {DOT_MIL_545LMG_0,GRE_MIL_0};
-	linkedItems[] = {LINKED_ITEMS_MIL};
-	respawnLinkedItems[] = {LINKED_ITEMS_MIL};
+	linkedItems[] = {LINKED_ITEMS_BASE};
+	respawnLinkedItems[] = {LINKED_ITEMS_BASE};
 	backpack = "haxey_O_rhs_rpg_AT";
 };
 class haxey_Milice_Taliban_Infantry_HG: O_HeavyGunner_F
@@ -170,7 +170,7 @@ class haxey_Milice_Taliban_Infantry_HG: O_HeavyGunner_F
 	faction = "haxey_faction_Milice_Taliban";
 	vehicleClass = "H_SuppMen";
 	displayName = "Villager HG";
-	uniformClass = "LOP_U_AM_Fatigue_01_2";
+	uniformClass = "btc_h_afghan_01";
 	class EventHandlers: EventHandlers
 	{
 		init = "if (local (_this select 0)) then {[(_this select 0), 'MIL_TAL'] call BG21_MG_fnc_random_gear;};";
@@ -181,8 +181,8 @@ class haxey_Milice_Taliban_Infantry_HG: O_HeavyGunner_F
 	respawnWeapons[] = {"rhs_weap_aks74u","Throw","Put"};
 	magazines[] = {DOT_MIL_545LMG_0,GRE_MIL_0};
 	respawnMagazines[] = {DOT_MIL_545LMG_0,GRE_MIL_0};
-	linkedItems[] = {LINKED_ITEMS_MIL};
-	respawnLinkedItems[] = {LINKED_ITEMS_MIL};
+	linkedItems[] = {LINKED_ITEMS_BASE};
+	respawnLinkedItems[] = {LINKED_ITEMS_BASE};
 	backpack = "RHS_DShkM_Gun_Bag";
 };
 class haxey_Milice_Taliban_Infantry_AHG: O_soldier_AAR_F
@@ -196,7 +196,7 @@ class haxey_Milice_Taliban_Infantry_AHG: O_soldier_AAR_F
 	faction = "haxey_faction_Milice_Taliban";
 	vehicleClass = "H_SuppMen";
 	displayName = "Villager AHG";
-	uniformClass = "LOP_U_AM_Fatigue_01_2";
+	uniformClass = "btc_h_afghan_01";
 	class EventHandlers: EventHandlers
 	{
 		init = "if (local (_this select 0)) then {[(_this select 0), 'MIL_TAL'] call BG21_MG_fnc_random_gear;};";
@@ -207,8 +207,8 @@ class haxey_Milice_Taliban_Infantry_AHG: O_soldier_AAR_F
 	respawnWeapons[] = {"rhs_weap_aks74u","Binocular","Throw","Put"};
 	magazines[] = {DOT_MIL_545LMG_0,GRE_MIL_0};
 	respawnmagazines[] = {DOT_MIL_545LMG_0,GRE_MIL_0};
-	linkedItems[] = {LINKED_ITEMS_MIL};
-	respawnLinkedItems[] = {LINKED_ITEMS_MIL};
+	linkedItems[] = {LINKED_ITEMS_BASE};
+	respawnLinkedItems[] = {LINKED_ITEMS_BASE};
 	backpack = "RHS_DShkM_TripodHigh_Bag";
 };
 class haxey_Milice_Taliban_Infantry_AB: O_Soldier_A_F
@@ -222,7 +222,7 @@ class haxey_Milice_Taliban_Infantry_AB: O_Soldier_A_F
 	faction = "haxey_faction_Milice_Taliban";
 	vehicleClass = "H_SuppMen";
 	displayName = "Carrier Villager";
-	uniformClass = "LOP_U_AM_Fatigue_01_2";
+	uniformClass = "btc_h_afghan_01";
 	class EventHandlers: EventHandlers
 	{
 		init = "if (local (_this select 0)) then {[(_this select 0), 'MIL_TAL'] call BG21_MG_fnc_random_gear;};";
@@ -233,8 +233,8 @@ class haxey_Milice_Taliban_Infantry_AB: O_Soldier_A_F
 	respawnWeapons[] = {"rhs_weap_m38","Throw","Put"};
 	magazines[] = {DOT_MIL_762R_0,GRE_MIL_0};
 	respawnmagazines[] = {DOT_MIL_762R_0,GRE_MIL_0};
-	linkedItems[] = {LINKED_ITEMS_MIL};
-	respawnLinkedItems[] = {"rhs_6b23_digi_6sh92_headset","rhs_6b27m_digi_ess",LINKED_ITEMS_MIL};
+	linkedItems[] = {LINKED_ITEMS_BASE};
+	respawnLinkedItems[] = {"rhs_6b23_digi_6sh92_headset","rhs_6b27m_digi_ess",LINKED_ITEMS_BASE};
 	backpack = "";
 };
 class haxey_Milice_Taliban_Infantry_rflm: O_soldier_F
@@ -248,7 +248,7 @@ class haxey_Milice_Taliban_Infantry_rflm: O_soldier_F
 	faction = "haxey_faction_Milice_Taliban";
 	vehicleClass = "H_Men";
 	displayName = "Villager";
-	uniformClass = "LOP_U_AM_Fatigue_01_2";
+	uniformClass = "btc_h_afghan_01";
 	class EventHandlers: EventHandlers
 	{
 		init = "if (local (_this select 0)) then {[(_this select 0), 'MIL_TAL'] call BG21_MG_fnc_random_gear;};";
@@ -259,8 +259,8 @@ class haxey_Milice_Taliban_Infantry_rflm: O_soldier_F
 	respawnWeapons[] = {"rhs_weap_m38","rhs_weap_rpg26","Throw","Put"};
 	magazines[] = {DOT_MIL_762R_0,GRE_MIL_0};
 	respawnmagazines[] = {DOT_MIL_762R_0,GRE_MIL_0};
-	linkedItems[] = {LINKED_ITEMS_MIL};
-	respawnLinkedItems[] = {"rhs_6b23_digi_6sh92_headset","rhs_6b27m_digi_ess",LINKED_ITEMS_MIL};
+	linkedItems[] = {LINKED_ITEMS_BASE};
+	respawnLinkedItems[] = {"rhs_6b23_digi_6sh92_headset","rhs_6b27m_digi_ess",LINKED_ITEMS_BASE};
 	backpack = "";
 };
 class haxey_Milice_Taliban_Infantry_lite_rflm: O_Soldier_lite_F
@@ -274,7 +274,7 @@ class haxey_Milice_Taliban_Infantry_lite_rflm: O_Soldier_lite_F
 	faction = "haxey_faction_Milice_Taliban";
 	vehicleClass = "H_Men";
 	displayName = "Villager lite";
-	uniformClass = "LOP_U_AM_Fatigue_01_2";
+	uniformClass = "btc_h_afghan_01";
 	class EventHandlers: EventHandlers
 	{
 		init = "if (local (_this select 0)) then {[(_this select 0), 'MIL_TAL'] call BG21_MG_fnc_random_gear;};";
@@ -285,7 +285,7 @@ class haxey_Milice_Taliban_Infantry_lite_rflm: O_Soldier_lite_F
 	respawnWeapons[] = {"rhs_weap_m38","Throw","Put"};
 	magazines[] = {DOT_MIL_762R_0,GRE_MIL_0};
 	respawnmagazines[] = {DOT_MIL_762R_0,GRE_MIL_0};
-	linkedItems[] = {LINKED_ITEMS_MIL};
-	respawnLinkedItems[] = {LINKED_ITEMS_MIL};
+	linkedItems[] = {LINKED_ITEMS_BASE};
+	respawnLinkedItems[] = {LINKED_ITEMS_BASE};
 	backpack = "";
 };
--- fnc/fn_random_gear.sqf
+++ fnc/fn_random_gear.sqf
@@ -8,17 +8,37 @@ switch (_this select 1) do {
 		private ["_headgears","_uniforms","_headgear","_uniform","_items","_blacklist_uniform"];
 	
 		_headgears = [
-			"LOP_H_Shemag_TAN","LOP_H_Pakol","LOP_H_Shemag_GRE","LOP_H_Turban","LOP_H_Turban_mask","LOP_H_Shemag_BLK"
+			"btc_h_afghan_01","H_Shemag_olive","btc_h_afghan_01_2","btc_h_afghan_01_3","btc_h_afghan_01_4","btc_h_afghan_01_5",
+			"btc_h_afghan_02","H_Shemag_olive","btc_h_afghan_02_2","btc_h_afghan_02_3","btc_h_afghan_02_4","btc_h_afghan_02_5",
+			"btc_h_afghan_03","H_ShemagOpen_tan","btc_h_afghan_03_2","btc_h_afghan_03_3","btc_h_afghan_03_4","btc_h_afghan_03_5",
+			"btc_h_afghan_04","H_ShemagOpen_khk","btc_h_afghan_04_2","btc_h_afghan_04_3","btc_h_afghan_04_4","btc_h_afghan_04_5",
+			"btc_h_afghan_05","btc_h_afghan_05_2","btc_h_afghan_05_3","btc_h_afghan_05_4","btc_h_afghan_05_5",
+			"btc_h_afghan_06","btc_h_afghan_06_2","btc_h_afghan_06_3","btc_h_afghan_06_4","btc_h_afghan_06_5"
 		];
 		_uniforms = [
-			"LOP_U_AM_Fatigue_01_2","LOP_U_AM_Fatigue_01_3","LOP_U_AM_Fatigue_01_4","LOP_U_AM_Fatigue_01_5","LOP_U_AM_Fatigue_01_6","LOP_U_AM_Fatigue_02_2","LOP_U_AM_Fatigue_02_3","LOP_U_AM_Fatigue_02_4","LOP_U_AM_Fatigue_02_5","LOP_U_AM_Fatigue_02_6","LOP_U_AM_Fatigue_03_2","LOP_U_AM_Fatigue_03_3","LOP_U_AM_Fatigue_03_4","LOP_U_AM_Fatigue_03_5","LOP_U_AM_Fatigue_03_6","LOP_U_AM_Fatigue_04_2","LOP_U_AM_Fatigue_04_3","LOP_U_AM_Fatigue_04_4","LOP_U_AM_Fatigue_04_5","LOP_U_AM_Fatigue_04_6"
+			"btc_u_afghan_01","btc_u_afghan_01_2","btc_u_afghan_01_3","btc_u_afghan_01_4","btc_u_afghan_01_5",
+			"btc_u_afghan_02","btc_u_afghan_02_2","btc_u_afghan_02_3","btc_u_afghan_02_4","btc_u_afghan_02_5",
+			"btc_u_afghan_03","btc_u_afghan_03_2","btc_u_afghan_03_3","btc_u_afghan_03_4","btc_u_afghan_03_5",
+			"btc_u_afghan_04","btc_u_afghan_04_2","btc_u_afghan_04_3","btc_u_afghan_04_4","btc_u_afghan_04_5",
+			"btc_u_afghan_05","btc_u_afghan_05_2","btc_u_afghan_05_3","btc_u_afghan_05_4","btc_u_afghan_05_5",
+			"btc_u_afghan_06","btc_u_afghan_06_2","btc_u_afghan_06_3","btc_u_afghan_06_4","btc_u_afghan_06_5",
+			"btc_u_soldier_01","btc_u_soldier_01_2","btc_u_soldier_01_2",
+			"btc_u_soldier_02","btc_u_soldier_02_2","btc_u_soldier_02_2",
+			"btc_u_soldier_03","btc_u_soldier_03_2","btc_u_soldier_03_2",
+			"btc_u_soldier_04","btc_u_soldier_04_2","btc_u_soldier_04_2",
+			"btc_u_soldier_05","btc_u_soldier_05_2","btc_u_soldier_05_2",
+			"btc_u_soldier_o_01","btc_u_soldier_o_01_2","btc_u_soldier_o_01_2",
+			"btc_u_soldier_o_02","btc_u_soldier_o_02_2","btc_u_soldier_o_02_2",
+			"btc_u_soldier_o_03","btc_u_soldier_o_03_2","btc_u_soldier_o_03_2",
+			"btc_u_soldier_o_04","btc_u_soldier_o_04_2","btc_u_soldier_o_04_2",
+			"btc_u_soldier_o_05","btc_u_soldier_o_05_2","btc_u_soldier_o_05_2"
 		];
 		
 		_uniform = _uniforms call BIS_fnc_selectRandom;
 		
 		_headgear = _headgears call BIS_fnc_selectRandom;
 		
-		_blacklist_uniform = [""];
+		_blacklist_uniform = ["btc_u_soldier_o_02","btc_u_soldier_o_02_2","btc_u_soldier_o_02_2","btc_u_soldier_02","btc_u_soldier_02_2","btc_u_soldier_02_2","btc_u_afghan_04","btc_u_afghan_04_2","btc_u_afghan_04_3","btc_u_afghan_04_4","btc_u_afghan_04_5","btc_u_afghan_05","btc_u_afghan_05_2","btc_u_afghan_05_3","btc_u_afghan_05_4","btc_u_afghan_05_5"];
 		
 		if (_uniform in _blacklist_uniform) then {_headgear = ""};
 		_items = uniformItems _unit;
